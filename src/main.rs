f146a400fce311396df0762151a57e424c413562
Andrew Mason <andrew@coderonfire.com>
2022-04-25 14:20:54 +0100

    Add URL encoding

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::io;
+use urlencoding::encode;
 
 fn validate_hash(hash: &str) {
     let length = hash.len();
@@ -9,6 +10,26 @@ fn validate_hash(hash: &str) {
     } else if !hash.chars().all(|c| c.is_ascii_hexdigit()) {
         panic!("Hash must be all hex digits, got {}", &hash);
     }
+}
+
+fn generate_magnet_link(hash: &str) -> String {
+    let sample = "
+    https://one.com
+    https://two.com
+    https://three.com
+    https://four.com
+    https://five.com
+    ";
+
+    let trackers = sample
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| l.trim())
+        .map(|l| format!("&tr={}", l));
+
+    let trackers = trackers.collect::<String>();
+
+    format!("magnet:?xt=urn:btih:{}{}", hash, trackers)
 }
 
 fn main() {
@@ -29,7 +50,11 @@ fn main() {
 
     let hash = hash.trim();
 
-    validate_hash(hash);
+    validate_hash(&hash);
 
-    println!("Hash is {}", hash);
+    println!("Hash is {}", &hash);
+
+    let link = generate_magnet_link(&hash);
+    let link = encode(&link).into_owned();
+    print!("Here's the link \n{}", link);
 }
